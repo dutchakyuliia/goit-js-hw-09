daf3b33e4f58f94fb1b78d04c93a2f6208260335
Dutchak Yuliia <dutchakyuliia@gmail.com>
2023-04-03 14:09:56 +0200

    resolve task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+const startBtn = document.querySelector('button[data-start]');
+const endBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 2000);
+  startBtn.setAttribute('disabled', true);
+  endBtn.removeAttribute('disabled');
+});
+endBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled');
+  endBtn.setAttribute('disabled', true);
+
+})
